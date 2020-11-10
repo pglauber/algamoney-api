1dca970f7df6f7245297c9d959a3e9b4f158baac
Paulo <pauloglauber@gmail.com>
2020-11-10 16:09:09 -0300

    Aula 3.11

---
+++ src/main/java/com/algamoneyapi/resource/PessoaResource.java
@@ -0,0 +1,49 @@
+package com.algamoneyapi.resource;
+
+import java.net.URI;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.algamoneyapi.model.Pessoa;
+import com.algamoneyapi.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		// Definindo o Location no Header para facilitar a utilização da API
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		if (pessoa != null) {
+			return ResponseEntity.of(pessoa);
+		}
+		return ResponseEntity.notFound().build(); 
+	}
+}
