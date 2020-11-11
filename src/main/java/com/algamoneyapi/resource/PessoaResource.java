6f7e76e0035c0af0dafe674a73b56a1b6003ed21
Paulo <pauloglauber@gmail.com>
2020-11-11 10:48:59 -0300

    Aula 4.1

--- src/main/java/com/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/algamoneyapi/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algamoneyapi.event.RecursoCriadoEvent;
@@ -45,5 +47,11 @@ public class PessoaResource {
 			return ResponseEntity.of(pessoa);
 		}
 		return ResponseEntity.notFound().build(); 
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 }
