7780bcb7bd6e3f36826513f7044a177996e9d065
pglauber <pauloglauber@gmail.com>
2021-03-08 20:31:44 -0300

    Aula 6.11

---
+++ src/main/java/com/algamoneyapi/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.algamoneyapi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algamoneyapi.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public Optional<Usuario> findByEmail(String email);
+}
